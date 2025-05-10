4be12e48c7e6f7e9046efa26ce232780546bcdcd
Shagun Garg <101324890+SHAGUN1936@users.noreply.github.com>
2025-05-10 21:45:13 +0530

    Update index.js

--- Introduction to Backend, NodeJs and Modules/ques_6/index.js
+++ Introduction to Backend, NodeJs and Modules/ques_6/index.js
@@ -3,14 +3,23 @@ const subtract = require("./subtraction");
 const multiply = require("./multiplication");
 const divide = require("./division");
 
-let sumResult = sum(3, 5);
-console.log("Sum:", sumResult);
+const [,, operation, a, b] = process.argv;
+const num1 = Number(a);
+const num2 = Number(b);
 
-let subResult = subtract(10, 4);
-console.log("Subtraction:", subResult);
-
-let mulResult = multiply(4, 6);
-console.log("Multiplication:", mulResult);
-
-let divResult = divide(12, 4);
-console.log("Division:", divResult);
+switch (operation) {
+  case 'sum':
+    console.log(sum(num1, num2));
+    break;
+  case 'subtract':
+    console.log(subtract(num1, num2));
+    break;
+  case 'multiply':
+    console.log(multiply(num1, num2));
+    break;
+  case 'divide':
+    console.log(divide(num1, num2));
+    break;
+  default:
+    console.log("Invalid operation. Use sum, subtract, multiply, or divide.");
+}
